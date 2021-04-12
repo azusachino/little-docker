7e88003d7aafd3a26405e2f0a787c9d9d663d936
ycpang <ycpang@iflytek.com>
2021-04-12 11:35:30 +0800

    feat. add springboot & node demo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # little-docker
+
 record the progress of learning docker
+
+`保证docker的2375端口可以访问`
+
+## springboot
+
+1. 采用spotify的部署插件
---
+++ simple-application/src/main/java/cn/az/app/simpleapplication/SimpleApplication.java
@@ -0,0 +1,17 @@
+package cn.az.app.simpleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author az
+ * @see cn.az.app.simpleapplication.controller.SimpleController
+ */
+@SpringBootApplication
+public class SimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApplication.class, args);
+    }
+
+}
